06043f143fed61cf657eef4726049b0eef3922ea
TanninOne <seppleviathan@gmx.de>
2021-07-14 13:56:49 +0200

    fixed build with ba2tk 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('gamebryo-ba2-support', __dirname, 4);
+const exp = webpack('gamebryo-ba2-support', __dirname, 4);
 
+exp.externals['./build/Release/ba2tk'] = './ba2tk';
+
+module.exports = exp;
